2c5347ee21f540f193e2f824812053b18833b67b
Mike Edwards <mikeedwards83@googlemail.com>
2012-03-04 19:12:47 +0000

    git razor commit

---
+++ Source/Glass.Sitecore.Mapper.Razor/IRazorControl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glass.Sitecore.Mapper.Razor
+{
+    public interface IRazorControl
+    {
+        string View
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ Source/Glass.Sitecore.Mapper.Razor/Web/Ui/TypedControl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glass.Sitecore.Mapper.Razor.Web.Ui
+{
+    public  class StrongControl<T> : AbstractRazorControl<T> where T:class
+    {
+
+        public override T GetModel()
+        {
+            ISitecoreContext _context = new SitecoreContext();
+            return _context.CreateClass<T>(false, false, GetDataSourceOrContext());
+        }
+    }
+}
