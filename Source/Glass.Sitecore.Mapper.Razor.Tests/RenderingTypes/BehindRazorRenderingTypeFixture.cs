e244c71273f6007f1a6c0112fee5e6bb0d4ad8ca
Laura Attew <l.attew@gmail.com>
2012-03-06 00:03:51 +0000

    Added BehindRazorRenderingTypeFixture

---
+++ Source/Glass.Sitecore.Mapper.Razor.Tests/RenderingTypes/BehindRazorRenderingTypeFixture.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glass.Sitecore.Mapper.Razor.Tests.RenderingTypes
+{
+    class BehindRazorRenderingTypeFixture
+    {
+    }
+}
